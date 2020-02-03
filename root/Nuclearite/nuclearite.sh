6c68a39deadbbfe70ff2b3511b0577c5653a7c80
adriengarcia34 <adrien.garcia34570@gmail.com>
2020-02-03 09:21:34 +0100

    sqlite moved to /var/www/

--- root/Nuclearite/nuclearite.sh
+++ root/Nuclearite/nuclearite.sh
@@ -3,11 +3,8 @@
 temp=$(cat /sys/bus/iio/devices/iio\:device0/in_temp_input)
 pressure=$(cat /sys/bus/iio/devices/iio\:device0/in_pressure_input)
 
-#temp=$(($temp/1000))
-#pressure=$(($pressure/1000))
-
 echo "$temp °C"
 echo "$pressure Pa"
 
-sqlite3 nuclearite.sqlite3 "INSERT INTO mesures(temperature, pressure, date_mesure, fk_env) VALUES (($temp/1000), ($pressure/1000), DATETIME(), 0);"
+sqlite3 /var/www/nuclearite.sqlite3 "INSERT INTO mesures(temperature, pressure, date_mesure, fk_env) VALUES (($temp/1000), ($pressure/1000), DATETIME(), 0);"
 echo "Data saved"
